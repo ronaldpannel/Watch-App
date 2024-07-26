50c19eb5d8edc44a97317fd59c146660d9d8dfa5
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2024-07-26 15:29:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ const secsEl = document.getElementById("sec");
 
 function upDateClock() {
   const currentDate = new Date();
-  setTimeout(() => {
-    const hour = currentDate.getHours();
-    const minute = currentDate.getMinutes();
-    const seconds = currentDate.getSeconds();
-    const hourDeg = (hour / 12) * 360;
-    hourEl.style.transform = `rotate(${hourDeg})`
-    const minutesDeg = (minute / 60) * 360;
-    minEl.style.transform = `rotate(${minutesDeg}deg)`;
-    const secondsDeg = (seconds / 60) * 360;
-    secsEl.style.transform = `rotate(${secondsDeg}deg)`;
+  setTimeout(upDateClock, 1000)
+   const hour = currentDate.getHours();
+   const minute = currentDate.getMinutes();
+   const seconds = currentDate.getSeconds();
+   const hourDeg = (hour / 12) * 360;
+   hourEl.style.transform = `rotate(${hourDeg}deg)`;
+   const minutesDeg = (minute / 60) * 360;
+   minEl.style.transform = `rotate(${minutesDeg}deg)`;
+   const secondsDeg = (seconds / 60) * 360;
+   secsEl.style.transform = `rotate(${secondsDeg}deg)`;
+   
 
-    console.log(hour, minute, seconds)
-    upDateClock();
-  }, 1000);
+    console.log(currentDate)
+  
 }
 
 upDateClock();
